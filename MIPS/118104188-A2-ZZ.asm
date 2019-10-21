68956c75498fd12957de4a000f1b0b2c28ef6948
Ziyan Zhang <44527704+Ziyan-Zhang1129@users.noreply.github.com>
2019-10-21 13:05:58 +0100

    New version

--- MIPS/118104188-A2-ZZ.asm
+++ MIPS/118104188-A2-ZZ.asm
@@ -1,16 +1,16 @@
 .data
 	inputStr: .space 20
 	str2float: .float
-	const10: .float 10
 	ask: .asciiz "$ Enter a real number:"
 	sign: .asciiz "$ The sign bit of your number is: "
+	error: .asciiz "invalid input! "
 	exponent_bin: .asciiz "\n$ The exponent of your number is: exponentBinary: "
-	display_hex: .asciiz "\texponentHex: "
-	space: .asciiz " "
-	hex: .asciiz "		"
-	to_hex: .asciiz "	"
-	f_bin: .asciiz "\n$The fraction of your number is: FractionBinary: "
 	f_hex: .asciiz"\tfractionHex: "
+	display_hex: .asciiz "\texponentHex: "
+	f_bin: .asciiz "\n$The fraction of your number is: fractionBinary: "
+	space: .asciiz " "
+	hex: .asciiz "			"
+	to_hex: .asciiz " "
 	
 .text
 .globl main
@@ -38,11 +38,19 @@ end_count:
 	
 convert_to_int:
 	lb $t0, inputStr($t1)
+	beq $t0, 45, negative	# 45 represents  '-' in ascii table
 	bne $t0, 46, push
 	addi $t1, $t1, 1
 	add $t5, $t1, $zero	# $t5 stores the R part
 	j convert_to_int
-push:
+negative:
+	add $t3, $t3, -1	# decrease length by 1
+	addi $t1, $t1, 1	# get rid of  '-' operator
+	li $t9, 1		# set $t9 as a negative flag, when a '-' is read, set flag to 1
+	j convert_to_int
+push:				# push every single number into stack
+	blt $t0,48,r_error
+	bgt $t0, 57, r_error
 	addi $t0, $t0, -48
 	addi $t1, $t1, 1
 	addi $sp, $sp, -4
@@ -54,7 +62,7 @@ end_push:
 	li $t2, 1
 	j pop
 	
-pop:
+pop:				# read from the stack
 	lw $t0, 0($sp)
 	addi $sp, $sp, 4
 	mul $t0, $t0, $t2
@@ -72,11 +80,15 @@ end_pop:
 	mtc1 $t2, $f1
 	cvt.s.w $f1, $f1
 	
-to_float:
+to_float:			# convert to float
 	div.s $f0, $f0, $f1
 	addi $t5, $t5, -1
-	beqz $t5, store_float
+	beqz $t5, check
 	j to_float
+
+check:
+	beqz $t9, store_float
+	neg.s $f0, $f0
 	
 store_float:
 	mtc1 $t4, $f3
@@ -165,15 +177,14 @@ print_hex_exponent:
 end_exponent:
 	li $t0, 0
 	li $t1, 23
-	li $t5, 0
-	li $t6, 0
+	li $t7, 0
 	li $v0, 4
 	la $a0, f_bin
 	syscall
 
 print_fraction:
 	mul $t0, $t0, 2
-	beqz $t1, print_hex_string
+	beqz $t1, print_hex_string2
 	addi $t1, $t1, -1
 	lw $t7, 0($sp)
 	add $t0, $t0, $t7
@@ -183,8 +194,60 @@ print_fraction:
 	syscall
 	div $t2, $t1, 4
 	mfhi $t3
-	beqz $t3, print_space
+	beqz $t3, print_space2
 	j print_fraction
+	
+print_space2:
+	li $v0, 4
+	la $a0, space
+	syscall
+	
+	blt $t0, 10, convert_to_hex2
+	addi $t0, $t0, 7
+
+convert_to_hex2:
+	addi $t0, $t0, 48
+	sb $t0, hex($t5)
+	li $t0, 0
+	addi $t5, $t5, 1
+	j print_fraction
+	
+print_hex_string2:
+	li $v0, 4
+	la $a0, f_hex
+	syscall
+	
+push_hex:
+	lb $t1, hex($t6)
+	addi $sp, $sp, -4
+	sw $t1, 0($sp)
+	
+	li $v0, 4
+	la $a0, 0($sp)
+	syscall
+	
+	div $t2, $t6, 2
+	mfhi $t3
+	beq  $t3, 1, call_space
+	
+	addi $t6, $t6, 1
+	beq $t6, $t5, end
+	j push_hex
+	
+call_space:
+	li $v0, 4
+	la $a0, space
+	syscall
+	
+	add $t6, $t6, 1
+	beq $t6, $t5, end
+	j push_hex
+
+r_error:
+	li $v0, 4
+	la $a0, error
+	syscall
+end:
 	
 
 
